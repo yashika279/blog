e23f6876028fa9b42d986c83f98cd06fcb8bfe2e
Yashika <yashikavijay2799@gmail.com>
2026-03-30 17:22:51 +0530

    Models added

--- src/main/java/com/yashika/blog/model/Article.java
+++ src/main/java/com/yashika/blog/model/Article.java
@@ -1,5 +1,30 @@
 package com.yashika.blog.model;
+import com.yashika.blog.model.Comment;
+import jakarta.persistence.*;
+import java.util.*;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String title;
+    private String content;
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Comment> comments;
+
+    public Article() {}
+
+    public Long getId() {return id; }
+    public void setId (Long id){ this.id = id;}
+
+    public String getTitle() {return title;}
+    public void setTitle(String title) {this.title = title;}
+
+    public String getContent() { return content; }
+    public void setContent(String content) {this.content = content;}
 }
--- src/main/java/com/yashika/blog/model/Comment.java
+++ src/main/java/com/yashika/blog/model/Comment.java
@@ -1,4 +1,30 @@
 package com.yashika.blog.model;
 
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    @JsonBackReference
+
+    private Article article;
+
+    public Comment() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text; }
+
+    public Article getArticle() { return article; }
+    public void setArticle(Article article) { this.article = article; }
 }
