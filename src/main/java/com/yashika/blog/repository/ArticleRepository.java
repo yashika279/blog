ffeec88fb07660918744f02831bc3d0aff4c3635
Yashika <yashikavijay2799@gmail.com>
2026-03-30 17:37:39 +0530

    Repository

--- src/main/java/com/yashika/blog/repository/ArticleRepository.java
+++ src/main/java/com/yashika/blog/repository/ArticleRepository.java
@@ -1,4 +1,7 @@
 package com.yashika.blog.repository;
 
-public class ArticleRepository {
+import com.yashika.blog.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long>{
 }
--- src/main/java/com/yashika/blog/repository/CommentRepository.java
+++ src/main/java/com/yashika/blog/repository/CommentRepository.java
@@ -1,4 +1,6 @@
 package com.yashika.blog.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class CommentRepository {
+import com.yashika.blog.model.Comment;
+public interface CommentRepository extends JpaRepository<Comment, Long>{
 }
